26f278059811dbbff75942d9c095f7678fe3a44d
Eduardo Martinez <eduardo.martinez66@hotmail.com>
2026-08-08 23:13:31 -0300

    Correccion errores

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -123,7 +123,6 @@ export async function generateMetadata({
         { url: "/favicon.svg", type: "image/svg+xml" },
         { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
         { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
-        { url: "/favicon.ico", sizes: "any" },
       ],
       apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
     },
